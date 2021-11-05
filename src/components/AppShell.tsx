750fa5cf68465717a3656b310394f9aa48c79da4
Paddy Hamilton <paddy@paddyhamilton.com>
2021-11-05 23:46:53 +0000

    chore:  working

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,18 +1,35 @@
+import { useEffect } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import cx from 'classNames';
+
+import { AppState } from '@utils/store';
+import { useAppSelector, useAppDispatch } from '@utils/hooks';
+
+import ThemeToggle from '@components/ThemeToggle';
 
 import styles from '../styles/Home.module.css';
 
 const IndexPage: NextPage = () => {
+  const { dark } = useAppSelector((state: AppState) => state.ui);
+
+  useEffect(() => {
+    const body = document.getElementsByTagName('body');
+    body[0].classList.toggle('dark');
+    return () => {};
+  }, [dark]);
+
   return (
-    <div className={styles.container}>
+    <div className={cx(styles.container, { [styles.dark]: dark })}>
       <Head>
         <title>NextJs no SSR boilerplate </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles.header}>
-        <p>Next Js no SSR boilerplate with Redux toolkit in Typescript</p>
-      </header>
+
+      <p>Next Js no SSR boilerplate with Redux toolkit in Typescript</p>
+      <ThemeToggle />
+      <p>Also use redux-persist for ... persisting state &#128522;</p>
+      <p>This also has a very contrived and not good use of redux.</p>
     </div>
   );
 };
